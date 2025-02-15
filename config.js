a748ac14d63dbce3e9a2daf407669733e8b46070
rawstarrat <newmna38@gmail.com>
2025-02-15 13:53:53 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  BOT_TOKEN: "7609874976:AAEoDb_x7MhhQmrvJxiXpxmB3VD_cJJ9SnE", // Token bot Telegram
+  OWNER_ID: ["7099464128"],
+  allowedGroupIds: [-1002401306074, -1002361556670, -1002386380830, -1002415460020, -4514397992], // ID grup yang diizinkan
+};
